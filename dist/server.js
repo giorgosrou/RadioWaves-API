6267896bae23250b1fa08f83902ed0d21d2881e5
Georgios Rounis <georg.rounis@gmail.com>
2024-04-09 16:12:41 +0200

    Fix configuration files, scripts for dev and production and file structure

---
+++ dist/server.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const radio_browser_api_1 = require("radio-browser-api");
+const app = (0, express_1.default)();
+const port = 3002;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get('/api/servers', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const genre = req.query.genre;
+        const api = new radio_browser_api_1.RadioBrowserApi('My Radio App');
+        const stationsData = yield api.searchStations({
+            language: 'english',
+            tag: genre,
+            limit: 50,
+        });
+        res.json(stationsData);
+    }
+    catch (error) {
+        console.error('Error fetching data from Radio Browser API:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
